310c6230321a59c2320580c09f353d57ca8103ed
Brunocor26 <bruno.correia@ubi.pt>
2025-12-02 11:34:47 +0000

    Corrigidas sondagens

--- src/models/Sondagem.js
+++ src/models/Sondagem.js
@@ -1,18 +1,51 @@
 const mongoose = require('mongoose');
 
 const sondagemSchema = mongoose.Schema({
-    titulo: {
+    entidade: {
         type: String,
-        required: true
+        required: false
     },
-    opcoes: {
-        type: [String],
-        required: true
+    tam_amostra: {
+        type: Number,
+        required: false
+    },
+    data_inicio_recolha: {
+        type: String, // or Date
+        required: false
+    },
+    data_fim_recolha: {
+        type: String, // or Date
+        required: false
+    },
+    metodologia: {
+        type: String,
+        required: false
+    },
+    universo: {
+        type: String,
+        required: false
+    },
+    margem_erro: {
+        type: Number,
+        required: false
+    },
+    nivel_confianca: {
+        type: Number,
+        required: false
     },
     resultados: {
         type: Map,
         of: Number,
         default: {}
+    },
+    resultado_dist_indecisos: {
+        type: Map,
+        of: Number,
+        required: false
+    },
+    titulo: { // Keeping title as it might be useful, or should I remove? The image doesn't show it explicitly but usually polls have titles. I'll keep it optional or remove if strict.
+        type: String,
+        required: false
     },
     dataCriacao: {
         type: Number,
